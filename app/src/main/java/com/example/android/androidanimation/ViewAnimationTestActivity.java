c566e0cc133ffd9e57895c43b2b5db4874ea41af
niedaocai <niedaocai@snda.com>
2016-11-28 23:16:00 +0800

    旋转动画（相对） 位移动画 缩放动画（绝对，相对）

--- app/src/main/java/com/example/android/androidanimation/ViewAnimationTestActivity.java
+++ app/src/main/java/com/example/android/androidanimation/ViewAnimationTestActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 public class ViewAnimationTestActivity extends AppCompatActivity
         implements View.OnClickListener {
@@ -16,6 +19,10 @@ public class ViewAnimationTestActivity extends AppCompatActivity
         setContentView(R.layout.activity_view_animation_test);
         findViewById(R.id.btn_alpha_animation).setOnClickListener(this);
         findViewById(R.id.btn_rotate_animation).setOnClickListener(this);
+        findViewById(R.id.btn_rotate_animation_rel).setOnClickListener(this);
+        findViewById(R.id.btn_translate_animation).setOnClickListener(this);
+        findViewById(R.id.btn_scale_animation).setOnClickListener(this);
+        findViewById(R.id.btn_scale_animation_rel).setOnClickListener(this);
     }
     
     @Override
@@ -27,9 +34,35 @@ public class ViewAnimationTestActivity extends AppCompatActivity
                 v.startAnimation(aa);
                 break;
             case R.id.btn_rotate_animation:
+                /*相对绝对位置（100，100）旋转*/
                 RotateAnimation ra = new RotateAnimation(0, 360, 100, 100);
                 ra.setDuration(3000);
                 v.startAnimation(ra);
+                break;
+            case R.id.btn_rotate_animation_rel:
+                /*相对自己的70%的位置旋转*/
+                RotateAnimation ra2 = new RotateAnimation(0, 360,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.7f,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.7f);
+                ra2.setDuration(3000);
+                v.startAnimation(ra2);
+                break;
+            case R.id.btn_translate_animation:
+                TranslateAnimation ta = new TranslateAnimation(0, 200, 0, 300);
+                ta.setDuration(3000);
+                v.startAnimation(ta);
+                break;
+            case R.id.btn_scale_animation:
+                ScaleAnimation sa = new ScaleAnimation(0, 2, 0, 2);
+                sa.setDuration(3000);
+                v.startAnimation(sa);
+                break;
+            case R.id.btn_scale_animation_rel:
+                ScaleAnimation sa2 = new ScaleAnimation(0, 1, 0, 1,
+                        Animation.RELATIVE_TO_SELF, 0.3f,
+                        Animation.RELATIVE_TO_SELF, 0.3f);
+                sa2.setDuration(3000);
+                v.startAnimation(sa2);
                 break;
             default:
                 Log.e(TAG, "onClick: ");
